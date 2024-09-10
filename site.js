1556465291e666f870d2c01af3605cf82fcee8ec
Brendan Philippe <500196562@fvtc.edu>
2024-09-10 00:38:40 -0500

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,16 @@
-console.log('Test')
+const welcomeMessage = document.querySelector('#welcome'); //grab welcome div
+const welcomeMessageH1 = welcomeMessage.querySelector('h1'); //grab welcome div
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if (isMorning) {
+    welcomeMessageH1.textContent = "Good Morning"
+} else if (isAfternoon) {
+    welcomeMessageH1.textContent = "Good Afternoon"
+} else if (isEvening) {
+    welcomeMessageH1.textContent = "Good Evening"
+}
