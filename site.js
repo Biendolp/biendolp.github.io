cfd62072bc7f24afe989dd2cc142b53abfa12c98
Brendan Philippe <500196562@fvtc.edu>
2024-09-16 14:32:49 -0500

    Added secret message

--- site.js
+++ site.js
@@ -1,3 +1,5 @@
+
+//Dynamic welcome, week 3
 const welcomeMessage = document.querySelector('#welcome'); //grab welcome div
 const welcomeMessageH1 = welcomeMessage.querySelector('h1'); //grab welcome div
 
@@ -14,3 +16,19 @@ if (isMorning) {
 } else if (isEvening) {
     welcomeMessageH1.textContent = "Good Evening"
 }
+
+//week 4 secret messages
+const key = 'user input'
+const span = document.querySelector('span')
+const userInput = document.querySelector('#input')
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    localStorage.setItem(key, userInput.value)
+})
+
+if (localStorage.getItem(key) == "It's a secret to everybody.") {
+    localStorage.setItem("Secret Message", "Zelda?")
+}
+const storedValue = localStorage.getItem("Secret Message") || 'Enter Secret Code'
+span.textContent = storedValue
