a7c7eb4773bbdabfe58dfed44507d784b05057e2
Brendan Philippe <500196562@fvtc.edu>
2024-10-04 22:28:35 -0500

    Added carousel

--- site.js
+++ site.js
@@ -19,3 +19,52 @@ if (isMorning) {
 
 //week 4 secret messages
 localStorage.setItem("Secret", "It's a secret to everybody.")
+
+//WEEK 6
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+setInterval(() => {
+    // code to run EVERY 5 seconds
+    currentImage += 1
+    if (currentImage >= 5){
+        currentImage = 0
+    }
+    showImages()
+}, 5000)
+
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
+prev.addEventListener('click', () => {
+    currentImage -= 1;
+    if (currentImage < 0) {
+        currentImage = 4;
+    }
+    showImages();
+});
+
+next.addEventListener('click', () => {
+    currentImage += 1;
+    if (currentImage >= 5) {
+        currentImage = 0;
+    }
+    showImages();
+});
